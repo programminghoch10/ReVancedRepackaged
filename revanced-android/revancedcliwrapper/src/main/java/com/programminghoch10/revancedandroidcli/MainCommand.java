c9346e1f0db058481a73b9dbb47ed4ed72063caf
programminghoch10 <16062290+programminghoch10@users.noreply.github.com>
2023-12-12 14:46:46 +0100

    decrease logging

--- revanced-android/revancedcliwrapper/src/main/java/com/programminghoch10/revancedandroidcli/MainCommand.java
+++ revanced-android/revancedcliwrapper/src/main/java/com/programminghoch10/revancedandroidcli/MainCommand.java
@@ -1,21 +1,20 @@
 package com.programminghoch10.revancedandroidcli;
 
-import java.lang.reflect.Method;
 import app.revanced.cli.command.MainCommandKt;
 
 public class MainCommand{
     public static void main(String[] args) {
-        System.out.println("sysout");
-        System.err.println("syserr");
+        //System.out.println("sysout");
+        //System.err.println("syserr");
         Runtime runtime = Runtime.getRuntime();
         long maxMemory = runtime.maxMemory();
-        System.err.println("Memory limit: " + maxMemory);
+        //System.err.println("Memory limit: " + maxMemory);
         try {
-            System.err.println("property");
+            //System.err.println("property");
             System.setProperty("java.io.tmpdir", "/data/local/tmp");
-            System.err.println("invoke");
+            //System.err.println("invoke");
             MainCommandKt.main(args);
-            System.err.println("end");
+            //System.err.println("end");
             System.exit(0);
         } catch (Exception e) {
             System.err.println("error");
