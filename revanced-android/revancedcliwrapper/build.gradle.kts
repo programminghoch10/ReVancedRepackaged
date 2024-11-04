1da6b9cbb55a2e312ed79e3a432d5770b2c563b0
programminghoch10 <16062290+programminghoch10@users.noreply.github.com>
2024-11-04 14:54:56 +0100

    update revancedcliwrapper targetSdk

--- revanced-android/revancedcliwrapper/build.gradle.kts
+++ revanced-android/revancedcliwrapper/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.programminghoch10.revancedandroidcli"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.programminghoch10.revancedandroidcli"
         minSdk = 23
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
     }
@@ -19,10 +19,6 @@ android {
             isMinifyEnabled = false
             signingConfig = signingConfigs["debug"]
         }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
     }
 }
 
